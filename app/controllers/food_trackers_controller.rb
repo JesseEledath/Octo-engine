7e1de7c83b913a231637c88eea099f080a4a52dc
rehack65 <dojojesse@gmail.com>
2021-04-04 16:10:19 -0400

    filled out the rest of the contreollers

--- app/controllers/food_trackers_controller.rb
+++ app/controllers/food_trackers_controller.rb
@@ -1,2 +1,44 @@
 class FoodTrackersController < ApplicationController
+  before_action :set_food_tracker, only: [:show, :update, :destroy]
+
+  def index
+    @food_trackers = FoodTracker.all
+    render json: @food_trackers
+  end
+
+  def show
+    render json: @food_tracker
+  end
+
+  def create
+    @food_tracker = FoodTracker.new(food_tracker_params)
+
+    if @food_tracker.save
+      render json: @food_tracker, status: :created
+    else
+      render json: @food_tracker.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @food_tracker.update(food_tracker_params)
+      render json: @food_tracker
+    else
+      render json: @food_tracker.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food_tracker.destroy
+  end
+
+  private
+
+  def set_food_tracker
+    @food_tracker = FoodTracker.find(params[:id])
+  end
+
+  def food_tracker_params
+    params.require(:food_tracker).permit(:name, :breed)
+  end
 end
