e0154b562d281e7dc3b4da70122707b3653a6bdc
rehack65 <dojojesse@gmail.com>
2021-04-05 16:30:54 -0400

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,51 @@
-import { Switch } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import "./App.css";
 import Layout from '../src/Components/Layout/Layout'
 import Landing from './Screens/Landing'
+import Login from './Screens/Login';
+import Register from './Screens/Register'
 // Imports =========================================================
+import { loginUser, registerUser, removeToken, verifyUser } from './Services/auth';
+
 function App() {
-  return (
-    <div className='layout-contianer'>
-      <Layout>
-        <Switch>
-        <Landing />
-        </Switch>
-      </Layout>
-    </div>
-  );
-}
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    }
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleRegister = async (formData) => {
+    const userData = await registerUser(formData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  }
+    return (
+      <div className='layout-contianer'>
+        <Layout>
+          <Switch>
+            <Landing />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 
 export default App;
