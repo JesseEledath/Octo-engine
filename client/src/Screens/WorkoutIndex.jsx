30e64401e0f43430fb01ba18e3f489fa5862c97f
rehack65 <dojojesse@gmail.com>
2021-04-09 13:49:16 -0400

    fix auth

--- client/src/Screens/WorkoutIndex.jsx
+++ client/src/Screens/WorkoutIndex.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { getAllWorkouts } from '../Services/workout'
-import { useState, useEffect } from 'react';
+import React from "react";
+import { getAllWorkouts } from "../Services/workout";
+import { useState, useEffect } from "react";
 function WorkoutIndex(props) {
   // State
   const [workouts, setWorkouts] = useState([]);
+  const { currentUser } = props;
   // Getting data
   useEffect(() => {
-    const fetchFoods = async () => {
+    const fetchWorkouts = async () => {
       const workoutData = await getAllWorkouts();
       setWorkouts(workoutData);
+    };
+    if (currentUser) {
+      fetchWorkouts();
     }
-    fetchFoods();
-  }, [])
+  }, [currentUser]);
 
-
-return (
+  return (
     <div>
-    <h3>Workouts</h3>
-    {
-      workouts.map((workout) => 
+      <h3>Workouts</h3>
+      {workouts.map((workout) => (
         <div className="workout-container" key={workout.id}>
           <h3>{workout.category}</h3>
-          <p>{ workout.content}</p>
+          <p>{workout.content}</p>
           <p>Do for {workout.time}</p>
           <p>Will burn roughly {workout.calories}</p>
-          </div>
-      )
-    }
+        </div>
+      ))}
     </div>
   );
 }
